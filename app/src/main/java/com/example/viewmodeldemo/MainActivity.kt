d0b9251ddb442afe1fad02872813eb74af3cf34c
Danish <iamdanishk555@gmail.com>
2022-06-12 21:35:40 +0545

    simple viewmodel example implemented

--- app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -3,21 +3,23 @@ package com.example.viewmodeldemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodeldemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.tvCount.text = count.toString()
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.tvCount.text = viewModel.getCurrentCount().toString()
 
         binding.btnClick.setOnClickListener {
-            count++
-            binding.tvCount.text = count.toString()
+            binding.tvCount.text = viewModel.getUpdatedCount().toString()
         }
     }
 }
