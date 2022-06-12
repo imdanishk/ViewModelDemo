38c74b7b5cece8a0ab86d3fe85d660e6f5d34ecb
Danish <iamdanishk555@gmail.com>
2022-06-12 21:26:18 +0545

    unused classes deleted

---
+++ app/src/main/java/com/example/viewmodeldemo/MainActivityViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.viewmodeldemo
+
+class MainActivityViewModel {
+}
--- app/src/main/java/com/example/viewmodeldemo/ViewModelExampleViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.viewmodeldemo
-
-import androidx.lifecycle.ViewModel
-
-class ViewModelExampleViewModel : ViewModel() {
-}
